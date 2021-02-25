5fd55b3b1e9a8ef3f88f3e2e2502a16700699305
Mikalai Butski <77263449+boksic04@users.noreply.github.com>
2021-02-25 22:40:56 +0300

    Add files via upload
    upload index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+module.exports = function towelSort (matrix) {
+  let arr = new Array();
+  
+  if(typeof matrix == "undefined" || matrix.length == 0){
+    return arr;
+  }
+  
+  for(let i = 0; i < matrix.length; i++) {
+    if(i%2 == 0) 
+    for(let n = 0; n < matrix[i].length; n++) {
+       arr.push(matrix[i][n]);
+    } else {
+      for(let n = (matrix[i].length-1); n >= 0; n--) {
+        arr.push(matrix[i][n]);
+    }  
+  }
+}
+ return arr;
 }
